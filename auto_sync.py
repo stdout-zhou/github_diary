affa6362f10f66f970ccb0a0db7d02f0c6ee37d2
decaku <38696173+Decaku@users.noreply.github.com>
2022-01-09 22:51:57 +0800

    update test2 diary

--- auto_sync.py
+++ auto_sync.py
@@ -1,21 +1,34 @@
 from datetime import datetime
+from email.mime.text import MIMEText
 import os
 import subprocess
 
 DATE_FLAG_DIR = 'date_flag'
+EMAIL_SENDER = 'stdout@bot.com'
 
 def _git_push(current_date):
-    add_command = ['git', 'add', '.']
-    commit_command = ['git', 'commit', '-m', 'update {} diary'.format(current_date)]
-    push_command = ['git', 'push']
-    subprocess.run(add_command + commit_command + push_command,
-                   shell=True,
-                   check=True)
+    add_command = 'git add .'
+    commit_command = 'git commit -m \'update {} diary\''.format(current_date)
+    push_command = 'git push'
+    for command in (add_command, commit_command, push_command):
+        subprocess.run(command,
+                       shell=True,
+                       check=True,
+                       stderr=subprocess.STDOUT)
 
-def _send_email():
+def _send_email(current_date):
     admin_email = os.environ['ADMIN_EMAIL']
-    print(admin_email)
-    
+
+    email_subject = '{} has been sync to github successfully!'.format(current_date)
+    email_content = ''
+    msg = MIMEText(email_content, 'html', 'utf-8')
+    msg['From'] = EMAIL_SENDER
+    msg['To'] = admin_email
+    msg['Subject'] = email_subject
+    p = subprocess.Popen(['/usr/sbin/sendmail', '-t'], stdin=subprocess.PIPE)
+    p.communicate(bytes(msg.as_string(), encoding='utf8'))
+
+
 def main():
     current_date = datetime.today().strftime('%Y-%m-%d')
     current_date_file_path = os.path.join(DATE_FLAG_DIR, current_date)
@@ -23,10 +36,10 @@ def main():
         with open(current_date_file_path, 'a'):
             os.utime(current_date_file_path, None)
         _git_push()
+        _send_email()
         
     else:
         print('exist')
 
 if __name__ == '__main__':
     main()
-    _send_email()
